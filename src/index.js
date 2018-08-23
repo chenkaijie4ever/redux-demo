33d4bda96972825b04de48902da9017ef729e203
chenkaijie4ever <chenkaijie4ever@gmail.com>
2018-08-23 17:20:25 +0800

    项目雏形

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+document.getElementById('addBtn').onclick = addTask
+document.getElementById('removeBtn').onclick = removeTask
+document.getElementById('clearBtn').onclick = clearTask
 
-ReactDOM.render(<div>MyRedux.</div>, document.getElementById('root'));
+
+const appState = {
+    taskCount: 0
+}
+
+function render(appState) {
+    const content = document.getElementById('content')
+    let taskCount = appState.taskCount
+    content.innerHTML = '当前有需求：' + taskCount + '个'
+}
+
+function addTask() {
+    appState.taskCount = appState.taskCount + 1
+    render(appState)
+}
+
+function removeTask() {
+    appState.taskCount = appState.taskCount - 1
+    render(appState)
+}
+
+function clearTask() {
+    appState.taskCount = 0
+    render(appState)
+}
