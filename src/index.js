9161ad693d12167da32932bba9efd652eee03125
chenkaijie <chenkaijie4ever@gmail.com>
2018-08-23 22:36:00 +0800

    创建Store来统一管理状态

--- src/index.js
+++ src/index.js
@@ -7,23 +7,55 @@ const appState = {
     taskCount: 0
 }
 
-function render(appState) {
+function actionHandler(state, action) {
+    switch (action.type) {
+        case 'ADD_TASK':
+            state.taskCount = state.taskCount + 1;
+            return state;
+        case 'REMOVE_TASK':
+            state.taskCount = state.taskCount - 1;
+            return state;
+        default:
+            console.log('有人要做骚操作')
+            return state;
+    }
+}
+
+function createStore(state, actionHandler) {
+
+    const getState = function () {
+        return state
+    }
+
+    const dispatch = function (action) {
+        state = actionHandler(state, action)
+    }
+
+    return { getState, dispatch }
+}
+
+const store = createStore(appState, actionHandler)
+
+function render() {
     const content = document.getElementById('content')
-    let taskCount = appState.taskCount
+    let taskCount = store.getState().taskCount
     content.innerHTML = '当前有需求：' + taskCount + '个'
 }
 
 function addTask() {
-    appState.taskCount = appState.taskCount + 1
-    render(appState)
+    let action = {type: 'ADD_TASK'}
+    store.dispatch(action)
+    render()
 }
 
 function removeTask() {
-    appState.taskCount = appState.taskCount - 1
-    render(appState)
+    let action = {type: 'REMOVE_TASK'}
+    store.dispatch(action)
+    render()
 }
 
 function clearTask() {
-    appState.taskCount = 0
-    render(appState)
+    let action = {type: 'CLEAR_TASK'}
+    store.dispatch(action)
+    render()
 }
